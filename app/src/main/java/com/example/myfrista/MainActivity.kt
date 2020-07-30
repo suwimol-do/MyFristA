bd15ac78628b420500c4d3b9ec0c5f01c8fab547
Suwimol <Suwimol.do@rmuti.ac.th>
2020-07-31 05:02:09 +0700

    add strings.xml

--- app/src/main/java/com/example/myfrista/MainActivity.kt
+++ app/src/main/java/com/example/myfrista/MainActivity.kt
@@ -1,11 +1,23 @@
-package com.example.myfrista
 
-import android.support.v7.app.AppCompatActivity
+package com.rashidcoder.kotlinand
+
+import android.app.Activity
 import android.os.Bundle
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import com.rashidcoder.kotlinandroid.R.layout.activity_main
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_drawer.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+    }
+    private  fun init (){
+        val toggle = ActionBarDrawerToggle(Activity(),drawer_layout,toolbar,R.string.nav_open,R.string.nav_close)
+
     }
 }
